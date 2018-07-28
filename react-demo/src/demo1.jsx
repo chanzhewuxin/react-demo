52668450625b89664e4b6e24e83fd687bb54c96e
paramita <cpeng0907@163.com>
2018-07-28 15:25:26 +0800

    upload files
    upload local file to server

---
+++ react-demo/src/demo1.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+
+export default class Welcome extends React.Component {
+
+    contructor(props) {
+        this.state = { name: 'a' }
+    }
+    // clickCallback() {
+    //     this.setState((preState, props) => {
+    //         console.log(preState);
+    //         return preState+'11111'
+    //     });
+    // }
+
+    // clickCallback=()=> {
+    //     this.setState((preState, props) => {
+    //         console.log(preState,props);
+    //         return preState+'11111';
+    //     });
+    // }
+
+    clickCallback() {
+        this.setState((preState, props) => {
+            console.log(preState);
+            return preState + '11111'
+        });
+    }
+
+    render() {
+        return <h1>Hello,
+            <button onClick={() => this.clickCallback()}>click</button>
+        </h1>;
+    }
+}
